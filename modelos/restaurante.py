dcfa62231d159755b029038e3311ee3da88372d4
Vitor Pellisoli <vitorpellisoli@gmail.com>
2024-07-03 20:13:40 -0300

    Envio completo

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,14 +1,40 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
-   nome = ""
-   categoria = ""
-   ativo = False
+   restaurantes = []
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = "Praça"
-restaurante_praca.categoria = "Gourmet"
+   def __init__(self, nome, categoria):
+      self._nome = nome.title()
+      self._categoria = categoria.upper()
+      self._ativo = False
+      self._avaliacao = []
+      Restaurante.restaurantes.append(self)
 
-restaurante_pizza = Restaurante()
+   def __str__(self):
+      return f'{self._nome} | {self._categoria}'
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+   @classmethod
+   def listar_restaurantes(cls):
+      print (f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
+      for restaurante in cls.restaurantes:
+         print(f"{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}")
 
-print(vars(restaurante_praca))
+   @property
+   def ativo(self):
+      return "Verdadeiro"if self._ativo else "False"
+
+   def alternar_estado(self):
+      self._ativo = not self._ativo
+
+   def receber_avaliacao(self, cliente, nota):
+      avaliacao = Avaliacao(cliente, nota)
+      self._avaliacao.append(avaliacao)
+
+   @property
+   def media_avalicoes(self):
+      if not self._avaliacao:
+         return 0
+      soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+      quantidade_de_notas = len(self._avaliacao)
+      media = round(soma_das_notas / quantidade_de_notas, 1)
+      return media
